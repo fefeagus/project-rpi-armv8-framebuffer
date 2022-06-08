bd705fec70aa5c51da42335b1372fa36d3e6666e
fefeagus <86791109+fefeagus@users.noreply.github.com>
2022-06-07 22:56:12 -0300

    nuevas animaciones y mas fichas corregidas

--- fichas.s
+++ fichas.s
@@ -178,35 +178,40 @@ ficha8:// ficha con forma de l invertida acostada a la derecha
 	ret
 
 ficha9:// ficha con forma de l invertida acostada a la izquierda
-	sub sp,sp,8
+	sub sp,sp,16
 	STUR x30, [SP, 0]
+	STUR x5,[SP,8]
 
 	bl ficha1
-	sub x6, x6, 30
 	add x5, x5, 30
 	bl ficha1
-	sub x6, x6, 30
 	add x5, x5, 30
 	bl ficha1
+	add x6, x6, 30
 	bl ficha1
 	
 	LDR x30, [SP, 0]
-	add sp,sp,8
+	LDR x5, [SP,8]
+	add sp,sp,16
 	ret
 
 ficha10:// ficha con forma de l invertida
-	sub sp,sp,8
+	sub sp,sp,16
 	STUR x30, [SP, 0]
-
+	STUR x5,[SP,8]
+	
 	bl ficha1
+	add x6, x6, 30
 	bl ficha1
+	add x6, x6, 30
 	bl ficha1
-	sub x6, x6, 30
 	sub x5, x5, 30
 	bl ficha1
+	sub x6, x6, 60
 	
 	LDR x30, [SP, 0]
-	add sp,sp,8
+	LDR x5, [SP,8]
+	add sp,sp,16
 	ret
 
 ficha11:// ficha con forma de l
@@ -242,8 +247,10 @@ ficha12:// ficha con forma de l acostada derecha
 	ret
 
 ficha13:// ficha con forma de l acostada izquierda
-	sub sp,sp,8
+	sub sp,sp,16
 	STUR x30, [SP, 0]
+	STUR x5,[SP,8]
+	
 
 	bl ficha1
 	sub x6, x6, 30
@@ -254,9 +261,11 @@ ficha13:// ficha con forma de l acostada izquierda
 	bl ficha1
 	sub x6, x6, 60
 	bl ficha1
+	add x6, x6, 30
 	
 	LDR x30, [SP, 0]
-	add sp,sp,8
+	LDR x5, [SP,8]
+	add sp,sp,16
 	ret
 	
 ficha14:// ficha con forma de t para arriba
@@ -338,35 +347,41 @@ ficha17:// ficha con forma de t lado derecho
 	ret
 
 ficha18:// ficha recta
-	sub sp,sp,8
+	sub sp,sp,16
 	STUR x30, [SP, 0]
+	STUR x5,[SP,8]
 
-	bl ficha1
-	bl ficha1
-	bl ficha1
-	bl ficha1
+	bl ficha2
+	add x6, x6, 30
+	bl ficha2
+	add x6, x6, 30
+	bl ficha2
+	add x6, x6, 30
+	bl ficha2
+	sub x6,x6,60
 	
 	LDR x30, [SP, 0]
-	add sp,sp,8
+	LDR x5, [SP,8]
+	add sp,sp,16
 	ret
 
 ficha19:// ficha recta acostada
-	sub sp,sp,8
+	sub sp,sp,16
 	STUR x30, [SP, 0]
-
-	bl ficha1
-	sub x6, x6, 30
+	STUR x5,[SP,8]
+	
+	bl ficha2
 	add x5, x5, 30
-	bl ficha1
-	sub x6, x6, 30
+	bl ficha2
 	add x5, x5, 30
-	bl ficha1
-	sub x6, x6, 30
+	bl ficha2
 	add x5, x5, 30
-	bl ficha1
+	bl ficha2
+	add x6, x6, 30
 	
 	LDR x30, [SP, 0]
-	add sp,sp,8
+	LDR x5, [SP,8]
+	add sp,sp,16
 	ret
 
 
@@ -476,4 +491,126 @@ Anim5: //parametros-> x19 de que lugar sale la ficha
 		add sp, sp, 8
 		ret
 
+Anim18: //parametros-> x19 de que lugar sale la ficha  
+	sub sp,sp,8
+	STUR x30, [SP, 0]
 
+	movz x4, 0x00, lsl 16  // color inicial de la linea
+	movk x4, 0x2000, lsl 0
+
+	mov x7,60		     //<-- en x7 voy guardando los parametros que se van pasando en x0 del rectangulo
+	mov x6,60
+
+
+	mov x14, 0    // iterador
+	
+	bl ficha19
+	mov x0, 1600
+	bl delay
+	bl sweepline120
+	bl ficha19
+	mov x0, 1600
+	bl delay
+	bl sweepline120
+	
+	loop_18:   //movimiento
+		bl ficha18
+
+		cmp x14, 2
+		b.eq ret18
+		add x14,x14,1
+
+		mov x0, 1600
+		bl delay
+
+		bl sweepline120
+
+		b loop_18
+	
+
+	
+	//recupero el registro x30 y retorno
+	ret18: 
+		LDR x30, [SP, 0]
+		add sp, sp, 8
+		ret
+
+	
+Anim19: //parametros-> x19 de que lugar sale la ficha  
+	sub sp, sp,8
+	STUR x30, [SP,0]
+
+	movz x4, 0x00, lsl 16  // color inicial de la linea
+	movk x4, 0x2000, lsl 0
+
+	mov x7,60		     //<-- en x7 voy guardando los parametros que se van pasando en x0 del rectangulo
+	mov x6,60
+
+
+	mov x14, 0    // iterador
+	
+	
+	
+	loop_19:   //movimiento
+		bl ficha19
+
+		cmp x14, 4
+		b.eq ret19
+		add x14,x14,1
+
+		mov x0, 1600
+		bl delay
+
+		bl sweepline120
+
+		b loop_19
+	
+
+	
+	//recupero el registro x30 y retorno
+	ret19: 
+		LDR x30, [SP, 0]
+		add sp, sp, 8
+		ret
+	
+
+Anim9_10: //parametros-> x19 de que lugar sale la ficha  
+	sub sp,sp,8
+	STUR x30, [SP, 0]
+
+	movz x4, 0x00, lsl 16  // color inicial de la linea
+	movk x4, 0x2000, lsl 0
+
+	mov x7,60		     //<-- en x7 voy guardando los parametros que se van pasando en x0 del rectangulo
+	mov x6,60
+
+
+	mov x14, 0    // iterador
+	
+	loop_9_10:   //movimiento
+		bl ficha9
+
+		cmp x14, 5
+		b.eq ret9_10
+		add x14,x14,1
+
+		mov x0, 1600
+		bl delay
+
+		bl sweepline
+
+		b loop_9_10	
+
+
+	
+	//recupero el registro x30 y retorno
+	ret9_10: 
+		mov x0, 1600
+		bl delay
+		bl sweepline
+		add x5, x5, 60
+		sub x6, x6, 30
+		bl ficha10
+		LDR x30, [SP, 0]
+		add sp, sp, 8
+		ret
